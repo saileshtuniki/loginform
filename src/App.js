e646a87ff5a5573e8452f10a4ffd34db27f93082
saileshtuniki <sailesh.tuniki@gmail.com>
2023-12-12 18:49:51 +0530

    basic loginform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+   
+  const [allEntry, setallEntry] = useState([]);
+
+  const submitform = (e)=>{
+    e.preventDefault();
+
+    if(email && password){
+      const newEntry = { id:new Date().getTime().toString(), email, password};
+      setallEntry([...allEntry, newEntry]);
+      console.log(allEntry);
+
+      setEmail("");
+      setPassword("");
+    }else{
+      alert("Enter the details !!")
+    } 
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> Basic Login Form validation </h3>
+      <form className='form' onSubmit={submitform}>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp" value={email} onChange={(e)=>setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1" value={password} onChange={(e)=>setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary mb-5">
+          Submit
+        </button>
+
+      </form>
+
+      <div className='datadisplay'>
+        {
+          allEntry.map((currentEle)=>{
+            //65 line --> Object destructuring//
+            const {id, email, password} = currentEle;
+            return(
+              <div className='container-row' key={id}>
+                <p> <b>Email:</b> {email} </p>
+                <p> <b>Password:</b>{password} </p>
+              </div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
